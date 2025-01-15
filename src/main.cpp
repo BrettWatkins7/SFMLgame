e95911d8922cf1e0639d0a3206129a934700f654
Brett Watkins <brettderek13147@gmail.com>
2025-01-14 21:15:18 -0600

    Changed rectangle color to green

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main()
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
     sf::RectangleShape rectangle(sf::Vector2f(128.0f, 128.0f));
-    rectangle.setFillColor(sf::Color::Red);
+    rectangle.setFillColor(sf::Color::Green);
     rectangle.setOrigin(sf::Vector2f(rectangle.getSize().x / 2, rectangle.getSize().y / 2));
     rectangle.setPosition(sf::Vector2f(100.0f, 100.0f));
 
